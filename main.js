354860e94cfc725c26b697d267b8d5357880a09a
SzybkiSzczypior <kalitar693@gmail.com>
2023-07-28 09:22:08 +0200

    --

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ const app = document.getElementById("app");
 const view = new notesView(app,{
     onNoteAdd() {
         console.log("Let's add a note! ");
+    },
+    onNoteEdit( newTitle , newBody ){
+        console.log(newTitle);
+        console.log(newBody);
     }
 })
+
--- notesView.js
+++ notesView.js
@@ -18,10 +18,29 @@ export default class notesView {
         `;
         const btnAddNote = this.root.querySelector(".notes__add");
         const inpTitle = this.root.querySelector(".notes__title");
-        const inpBody = this.root.querySelector("notes__body");
+        const inpBody = this.root.querySelector(".notes__body");
         btnAddNote.addEventListener("click",() => {
             this.onNoteAdd();
 
-        })
+        });
+        [ inpTitle , inpBody ].forEach(inputField => {
+            inputField.addEventListener("blur",() =>{
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
+
+                this.onNoteEdit(updatedTitle,updatedBody);
+            });
+        });
+    }
+    _createListItemHTML(id, title, body, updated) {
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+        <div class="notes__list-item" data-note-id="${id}">
+            <div class="notes__small-title" >${title}</div>
+            <div class="notes__small-body" >${body.substring}</div>
+            <div class="notes__small-updated" ></div>
+        </div>
+        `;
     }
 }
